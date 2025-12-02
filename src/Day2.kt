7a82875ee9d0b8bc0cd20e2c623df9742413153b
Sitamsh Rijal <sitamshrijal@gmail.com>
2025-12-02 11:13:52 +0545

    Add solution for Day 2 Part 1

---
+++ src/Day2.kt
@@ -0,0 +1,21 @@
+fun main() {
+    fun part1(input: List<String>): Long {
+        val idRanges = input[0].split(",").map {
+            val splits = it.split("-")
+            splits[0].toLong()..splits[1].toLong()
+        }
+        val regex = """(\d+)\1""".toRegex()
+        val invalidIds = idRanges.flatMap { range ->
+            range.filter { regex.matches("$it") }
+        }
+        return invalidIds.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val input = readInput("input2")
+    part1(input).println()
+    part2(input).println()
+}
