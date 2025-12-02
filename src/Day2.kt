9d7ee90e3f43f9404a01d207511c24e016798599
Sitamsh Rijal <sitamshrijal@gmail.com>
2025-12-02 17:35:13 +0545

    Add solution for Day 2 Part 2

--- src/Day2.kt
+++ src/Day2.kt
@@ -11,8 +11,16 @@ fun main() {
         return invalidIds.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val idRanges = input[0].split(",").map {
+            val splits = it.split("-")
+            splits[0].toLong()..splits[1].toLong()
+        }
+        val regex = """(\d+)\1+""".toRegex()
+        val invalidIds = idRanges.flatMap { range ->
+            range.filter { regex.matches("$it") }
+        }
+        return invalidIds.sum()
     }
 
     val input = readInput("input2")
